64f188a07b9b63c02dbcc3175d3d54a6059e63ca
jakeroose <jakeroose.b@gmail.com>
2019-05-03 09:27:40 -0400

    created seeds migration for generating fake timer data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+u = User.create!({:email => "generateduser@example.com", :password => "password", :password_confirmation => "password" })
+
+start_date = 1.week.ago
+descriptions = ['Project 1', 'Project 2', 'Exercise', 'Sleep', 'Relax', 'Guitar Practice']
+min_time = 20.minutes.seconds
+max_time = 8.hours.seconds
+21.times do |i|
+  tf = TimeFrame.create!({
+    user: u,
+    description: descriptions.sample,
+    time_elapsed: rand(min_time..max_time).to_i,
+    created_at: (start_date + rand(7.days))
+  })
+end
